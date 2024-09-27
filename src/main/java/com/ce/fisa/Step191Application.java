916bf582c193dbb08e6f93624d8602d9dc354e92
seungjibaek <seungji2001@gmail.com>
2024-09-27 17:03:11 +0900

    build

---
+++ src/main/java/com/ce/fisa/Step191Application.java
@@ -0,0 +1,13 @@
+package com.ce.fisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step191Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step191Application.class, args);
+	}
+
+}
